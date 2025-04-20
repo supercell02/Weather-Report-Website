73259338c60ba146eb1cc2887c0e15957345d574
Amar Kumar Thakur <107260222+supercell02@users.noreply.github.com>
2025-04-20 18:21:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "ea45cc19f2mshfc1dabd815c2b3ap1d2a90jsn1f5abb6da4f1",
-    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
-  },
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ea45cc19f2mshfc1dabd815c2b3ap1d2a90jsn1f5abb6da4f1',
+		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+	}
 };
 
 const getWeather =(city) =>{
     cityName.innerHTML = city
 fetch(
-  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,options)
+  "https://open-weather13.p.rapidapi.com/city/" + city,options)
   .then((response) => response.json())
   .then((response) => {
     console.log(response);
